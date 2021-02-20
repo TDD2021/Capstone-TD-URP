b96f0bf9e604a1387eb65fea0695d40b5c020869
Tim <tdonalddavis@yahoo.ca>
2021-02-20 07:48:42 -0500

    Simple hit detection 1
    Grid of cubes with towers and minion.
    Minion has fixed path, will trigger event when in proxy of towers.

---
+++ Capstone_TD_URP/Assets/Minions/CollisionHandler.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionHandler : MonoBehaviour
+{
+    /*private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(this.name + "--Collided with --" + collision.gameObject.name);
+    }*/
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log($"{this.name} **Entered Range of: ** {other.gameObject.name}");
+        other.gameObject.GetComponent<Renderer>().material.color = Color.green;
+    }
+
+    /*private void OnTriggerStay(Collider other)
+    {
+        Debug.Log($"{this.name} **In Range of: ** {other.gameObject.name}");
+    }*/
+
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log($"{this.name} **Left Range of: ** {other.gameObject.name}");
+        other.gameObject.GetComponent<Renderer>().material.color = Color.white;
+    }
+}
