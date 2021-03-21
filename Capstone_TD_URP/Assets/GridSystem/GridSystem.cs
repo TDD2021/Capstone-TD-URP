71a45c71ea390a9c20051fdbd02c1d153a705720
Greg Tatel <tatelgregory@gmail.com>
2021-03-21 14:33:04 -0400

    Added Sell button and started collision detection for towers
    I scaled the UI for mobile phones and as well added the collision detection to detect which object it collides with under GridSystem.cs.

--- Capstone_TD_URP/Assets/GridSystem/GridSystem.cs
+++ Capstone_TD_URP/Assets/GridSystem/GridSystem.cs
@@ -12,6 +12,9 @@ public class GridSystem : MonoBehaviour
     private Grid3D<GridObject> grid;
     private Ray ray;
     [SerializeField] private LayerMask mouseColliderLayerMask;
+    BuildManager buildManager;
+ 
+
 
     public class GridObject
     {
@@ -31,6 +34,11 @@ public class GridSystem : MonoBehaviour
         {
             this.transform = transform;
             grid.TriggerGridObjectChanged(x, z);
+        }
+
+        public Transform GetTransform()
+        {
+            return transform;
         }
 
         public void ClearTransform()
@@ -56,30 +64,76 @@ public class GridSystem : MonoBehaviour
         float cellSize = 5f;
 
         grid = new Grid3D<GridObject>(gridWidth, gridHeight, cellSize, Vector3.zero, (Grid3D<GridObject> g, int x, int z) => new GridObject(g, x, z));
+        buildManager = BuildManager.instance;
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        buildManager = BuildManager.instance;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //if (Input.GetMouseButtonDown(0))
+        //{
+        //    grid.GetXZ(Utility.GetMouseWorldPosition(mouseColliderLayerMask), out int x, out int z);
+
+        //    if (x >= 0 && z >= 0 && x < gridWidth && z < gridHeight)
+        //    {
+        //        GridObject gridObject = grid.GetGridObject(x, z);
+        //        if (gridObject.CanBuild())
+        //        {
+        //            Transform builtTransform = Instantiate(testTransform, grid.GetWorldPosition(x, z), Quaternion.identity);
+        //            gridObject.SetTransform(builtTransform);
+        //        }
+        //    }
+        //}
         if (Input.GetMouseButtonDown(0))
         {
+            //logic for selling tower using tags
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 999f, mouseColliderLayerMask)) {
+                Debug.Log(hit.collider.gameObject.tag);
+                Debug.Log(BuildManager.instance.GetSellTower());
+                if (hit.collider.gameObject.tag == "Tower" && BuildManager.instance.GetSellTower()) {
+                    Destroy(hit.transform.gameObject);
+                    BuildManager.instance.SetSellTower(false);
+                    Debug.Log(BuildManager.instance.GetSellTower());
+                }
+                else 
+                {
+                    Debug.Log("Did not sell");
+                }
+            }
+
+            if (buildManager.GetBuildTower() == null)
+                return;
+
             grid.GetXZ(Utility.GetMouseWorldPosition(mouseColliderLayerMask), out int x, out int z);
 
             if (x >= 0 && z >= 0 && x < gridWidth && z < gridHeight)
             {
                 GridObject gridObject = grid.GetGridObject(x, z);
+                Debug.Log("Can Build: "+ gridObject.CanBuild());
                 if (gridObject.CanBuild())
                 {
-                    Transform builtTransform = Instantiate(testTransform, grid.GetWorldPosition(x, z), Quaternion.identity);
-                    gridObject.SetTransform(builtTransform);
+                    GameObject towerToBuild = BuildManager.instance.GetBuildTower();
+                    Transform tower = Instantiate(towerToBuild.transform, grid.GetWorldPosition(x, z), Quaternion.identity);
+                    gridObject.SetTransform(tower);
+                    //Remove current selection
+                    BuildManager.instance.SetBuildTower(null);
+
                 }
+          
+                
+                 
+                
             }
         }
     }
+   
+
 }
---
+++ Capstone_TD_URP/Assets/Scripts/MapScripts/Tower1.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower1 : MonoBehaviour
+{
+    BuildManager buildManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        buildManager = BuildManager.instance;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
