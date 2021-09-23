ec604fdc35644c641ed1a4feb98f00be28b6f085
tahuy1992 <phuonghuy.ta@gmail.com>
2021-09-23 10:44:56 -0400

    Update Minion Collision Base

--- Capstone_TD_URP/Assets/Scripts/MinionScripts/MinionCollision_base.cs
+++ Capstone_TD_URP/Assets/Scripts/MinionScripts/MinionCollision_base.cs
@@ -1,22 +1,60 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MinionCollision_base : MonoBehaviour
 {
+    private float health;
+    public float startHealth = 100f;
+    public Image healthBar;
+
+    public GameObject impactEffect;
+
+    Vector3 basePosition = new Vector3();
 
 
-    private void OnCollisionEnter(Collision TheColliderThatIWillBeCollidingWith)
+    private float damage = 10f;
+
+    private void Start()
+    {
+        health = startHealth;
+        basePosition = transform.position;
+    }
+
+    void OnCollisionEnter(Collision TheColliderThatIWillBeCollidingWith)
     {
         //Debug.Log(TheColliderThatIWillBeCollidingWith.gameObject.name);
 
-        // if (TheColliderThatIWillBeCollidingWith.gameObject.CompareTag("Enemy"))
-        //{
-        //Destroy(TheColliderThatIWillBeCollidingWith.gameObject);
-        Destroy(TheColliderThatIWillBeCollidingWith.gameObject);
+        if (TheColliderThatIWillBeCollidingWith.gameObject.CompareTag("Enemy"))
+        {
 
-        // }
+            Destroy(TheColliderThatIWillBeCollidingWith.gameObject);
+            //Destroy(TheColliderThatIWillBeCollidingWith.gameObject);
+            takeDamage(damage);
+
+
+        }
 
     }
 
+    public void takeDamage(float damageAmount)
+    {
+        health -= damageAmount;
+
+        healthBar.fillAmount = health / startHealth;
+
+        if (health <= 0)
+        {
+            GameObject impactGO = Instantiate(impactEffect, basePosition, Quaternion.LookRotation(basePosition));
+            Destroy(impactGO, 0.4f);
+            die();
+
+        }
+    }
+    void die()
+    {
+        Destroy(gameObject);
+        Time.timeScale = 0;
+    }
 }
